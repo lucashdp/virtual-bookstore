fd720e5b30947a57002aa7c21cae563e3bf86628
Lucas Duarte <lucashdp@outlook.com>
2018-09-30 15:24:46 -0300

    refactor: refactor server settings and add routes in index
    Add the mongoDB connection in server.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,12 @@
-const bookRoutes = require('./books_routes');
+const booksRoutes = require('./books_routes');
 const authorsRoutes = require('./authors_routes');
+const ordersRoutes = require('./orders_routes');
+const publishersRoutes = require('./publishers_routes');
+const usersRoutes = require('./users_routes');
 module.exports = function (app, db) {
-  bookRoutes(app, db);
+  booksRoutes(app, db);
   authorsRoutes(app, db);
+  ordersRoutes(app, db);
+  publishersRoutes(app, db);
+  usersRoutes(app, db);
 };
--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
-const bodyParser     = require('body-parser');
-const app            = express();
+// server.js
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./app/config/db');
+const routes = require('./app/routes');
+const app = express();
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
-require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err)
+    routes(app, database);
+    app.listen(port, () => {
+        console.log('We are live on ' + port);
+    });
 });
